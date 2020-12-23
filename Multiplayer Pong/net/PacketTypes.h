06428efb551f075af540ac7471d8f4f27f432703
Chris <34993144+Mesiow@users.noreply.github.com>
2020-12-22 21:24:13 -0500

    add connect request enum

--- Multiplayer Pong/net/PacketTypes.h
+++ Multiplayer Pong/net/PacketTypes.h
@@ -2,13 +2,16 @@
 #include <SFML/Network/Packet.hpp>
 #include <cstdint>
 
-enum CommandToServer : uint8_t {
+enum class CommandToServer : uint8_t {
 	Connect = 0,
 	Disconnect = 1,
 };
 
-enum CommandToClient : uint8_t {
+enum class CommandToClient : uint8_t {
+	PaddleState = 0,
+	BallState = 1,
 
+	ConnectRequestResult
 };
 
 sf::Packet& operator<<(sf::Packet& packet, CommandToServer &command);
