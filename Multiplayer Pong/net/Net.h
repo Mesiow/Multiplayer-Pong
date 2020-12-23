5aca809777e35076057e8aaa2b612e343795ab4c
Chris <34993144+Mesiow@users.noreply.github.com>
2020-12-22 21:25:52 -0500

    virtual destructor

--- Multiplayer Pong/net/Net.h
+++ Multiplayer Pong/net/Net.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <SFML/Network.hpp>
 
+#define log(msg, address, port) std::cout<<msg<<" "<<address<<":"<<port<<"\n";
+#define clog(msg) std::cout<<msg<<"\n";
+#define log_error(msg) std::cout<<msg<<"\n";
+
 class Net {
 public:
 	Net() = default;
+	virtual ~Net() {}
 	virtual void receivePackets() = 0;
-	virtual void sendPacket(sf::Packet& packet) = 0;
+	virtual void sendPacket(sf::Packet& packet){}
 
 protected:
 	sf::UdpSocket socket_;
