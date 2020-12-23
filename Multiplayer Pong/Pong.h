b861e3706645dfc4f2f036cc08ade98874c55dc3
Chris <34993144+Mesiow@users.noreply.github.com>
2020-12-22 21:22:12 -0500

    add game constants

--- Multiplayer Pong/Pong.h
+++ Multiplayer Pong/Pong.h
@@ -1,3 +1,8 @@
 #pragma once
 
 constexpr unsigned char MAX_CONNECTIONS = 2;
+
+constexpr unsigned int SCREEN_WIDTH = 256;
+constexpr unsigned int SCREEN_HEIGHT = 240;
+
+constexpr unsigned int BALL_SIZE = 2;
