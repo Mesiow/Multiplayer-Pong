051802f2468552eeff1c5caec3720eee59e68e35
Chris <34993144+Mesiow@users.noreply.github.com>
2020-12-23 19:49:27 -0500

    set window size

--- Multiplayer Pong/Pong.h
+++ Multiplayer Pong/Pong.h
@@ -3,6 +3,6 @@
 constexpr unsigned char MAX_CONNECTIONS = 2;
 
 constexpr unsigned int SCREEN_WIDTH = 256;
-constexpr unsigned int SCREEN_HEIGHT = 240;
+constexpr unsigned int SCREEN_HEIGHT = 148;
 
 constexpr unsigned int BALL_SIZE = 2;
