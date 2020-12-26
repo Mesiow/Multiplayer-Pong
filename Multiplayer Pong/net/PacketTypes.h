40882997843675c49957d3d3dc4e59640d85d767
Chris <34993144+Mesiow@users.noreply.github.com>
2020-12-26 11:07:41 -0500

    Add input enum

--- Multiplayer Pong/net/PacketTypes.h
+++ Multiplayer Pong/net/PacketTypes.h
@@ -5,6 +5,8 @@
 enum class CommandToServer : uint8_t {
 	Connect = 0,
 	Disconnect = 1,
+
+	ClientInput
 };
 
 enum class CommandToClient : uint8_t {
@@ -12,6 +14,11 @@ enum class CommandToClient : uint8_t {
 	BallState = 1,
 
 	ConnectRequestResult
+};
+
+enum Input : uint8_t{
+	Up = 0x1,
+	Down = 0x2
 };
 
 sf::Packet& operator<<(sf::Packet& packet, CommandToServer &command);
